9e872c1dc660a1e005e72b1156052cfa3a28a203
Ganesh <gkunwar059@gmail.com>
2023-07-03 11:20:01 +0545

    Your commit message

---
+++ basic_01.py
@@ -0,0 +1,29 @@
+# sum of two number 
+# a= 4
+# b=7
+# sum = a+b
+# print(f"the sum is {sum}")
+
+# a =  int (input("Enter first Number :"))
+# b= int(input("enter second number :"))
+# sum= a+b
+# dif= a-b
+# print(f"The sum is {sum}")
+# print(f"diff is {dif}")
+
+# datatype of the python
+#  none is know as empty 
+# tuples 
+# list
+# dictionary
+# set
+
+
+#example of the array
+# a= "Ganeshkunwar"
+# print(a)
+# print(a[2])  this will find the location of array 0-1
+# print (a[-1])
+
+a= True
+print(type(a))
